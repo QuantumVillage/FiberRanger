702bab37d1e6ee5dc11bbd5ae9ec98ecc88f0fe3
Mark C. <idelta0@gmail.com>
2026-01-19 23:34:55 +0000

    Delete build directory

--- build/CMakeFiles/entropy_gen.dir/Users/mc/Software/pico-sdk/lib/mbedtls/library/pkcs11.c.o.d
+++
@@ -1,4 +0,0 @@
-CMakeFiles/entropy_gen.dir/Users/mc/Software/pico-sdk/lib/mbedtls/library/pkcs11.c.o: \
- /Users/mc/Software/pico-sdk/lib/mbedtls/library/pkcs11.c \
- /Users/mc/Software/pico-sdk/lib/mbedtls/include/mbedtls/pkcs11.h \
- /Users/mc/QuantumVillage/Fiber-Ranger/mbedtls_config.h
--- build/_deps/picotool-build/data_locs.cpp
+++
@@ -1,7 +0,0 @@
-
-#include <vector>
-#include <string>
-
-std::vector<std::string> data_locs = {
-    "./","/Users/mc/QuantumVillage/Fiber-Ranger/build/_deps/picotool/"
-};
